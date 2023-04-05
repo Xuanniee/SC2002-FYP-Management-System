578ccc7abd27e77fbfd0827876716cb9875e9ca7
Ken <kenfongyexuan@gmail.com>
2023-04-05 22:26:12 +0800

    Update Project.java
    Added getters and setters and display project info method

--- FYPMS/src/Project.java
+++ FYPMS/src/Project.java
@@ -21,7 +21,57 @@ public class Project {
         this.projectStatus = ProjectStatus.AVAILABLE;
     }
 
+    // Getters and Setters
+    // projectID
+    public int getProjectID() {
+        return this.projectID;
+    }
+
+    // supervisorName
+    public String getSupervisorName() {
+        return this.supervisorName;
+    }
+
+    public void setSupervisorName(String supervisorName) {
+        this.supervisorName = supervisorName;
+    }
+
+    // supervisorEmail
+    public String getSupervisorEmail() {
+        return supervisorEmail;
+    }
+
+    public void setSupervisorEmail(String supervisorEmail) {
+        this.supervisorEmail = supervisorEmail;
+    }
+
+    // projectTitle
+    public String getProjectTitle() {
+        return projectTitle;
+    }
+
     public void changeProjectTitle(String newProjectTitle) {
         this.projectTitle = newProjectTitle;
     } 
+
+    // projectStatus
+    public ProjectStatus getProjectStatus() {
+        return projectStatus;
+    }
+
+    public void setProjectStatus(ProjectStatus projectStatus) {
+        this.projectStatus = projectStatus;
+    }
+
+    // Method to display all information the project
+    public void displayProjectInfo() {
+        System.out.println("------------- PROJECT INFO -------------");
+        System.out.println("Project ID: " + this.projectID);
+        System.out.println("Project Title: " + this.projectTitle);
+        System.out.println("Project Status: " + this.projectStatus);
+        System.out.println("Supervisor Name: " + this.supervisorName);
+        System.out.println("Supervisor email: " + this.supervisorEmail);
+        System.out.println("----------------------------------------");
+    }
+
 }
